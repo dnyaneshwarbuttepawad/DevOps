60f261e831cf4e8fa6b464c75ad3fe13230ded06
Dnyaneshwar <dnyaneshwarbuttepawad@gmail.com>
2025-11-03 11:18:58 +0000

    Added sample.tf file

--- Terraform/sample.tf
+++ Terraform/sample.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  
+region = "eu-noutrh-1"
 }
-
-resource "" "name" {
-  
+resource "aws_instance" "myinstance" {
+ami = "ami-055e4d03ab1de5def"
+instance_type = "t2.micro"
+key_name = "id_rsa"
 }
